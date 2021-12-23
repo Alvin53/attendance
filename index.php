d39d7ec00d971ff0a0271137d3398fb405cbc9d8
Alvin53 <59268286+Alvin53@users.noreply.github.com>
2021-12-22 23:32:54 -0500

    update index page

--- index.php
+++ index.php
@@ -6,10 +6,35 @@
     $result = $crud->getSpecialties();
 
 ?>
-   
-     <h1 class="text-center">WELCOME TO THE CLUB/ SPORTS REGISTRATION </h1>
 
+     
+  <div class="container py-4">
+    <header class="pb-3 mb-4 border-bottom">
       
+    </header>
+
+    <div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">REGISTER FOR YOUR TEAM NOW</h1>
+        <p class="col-md-8 fs-4">Red house is the defending champions in the school football, be sure to pick the right team when registering.</p>
+        <a  href="login.php"><button class="btn btn-primary btn-lg"  type="button">Register Today</button></a>
+      </div>
+    </div>
+
+    <div class="row align-items-md-stretch">
+      <div class="col-md-6">
+        <div class="h-100 p-5 text-white bg-dark rounded-3">
+          <h2>DO YOU HAVE WHAT IT TAKES THIS SEASON</h2>
+          <p>Never try to quit, quiting is not an option in our book.</p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="h-100 p-5 bg-light border rounded-3">
+          <h2>BE SURE TO CONTACT US!</h2>
+          <p></p>
+        </div>
+      </div>
+    </div>
         <br>
         <br>
         <br>
--- sportregister.php
+++ sportregister.php
@@ -9,58 +9,62 @@
    
      <h1 class="text-center">CLUB/SPORT REGISTRATION</h1>
 
-        <form method="post" action="success.php" >
+     <div class="container">
+     <form method="post" action="success.php" >
 
-            <div class="mb-3">
-                <label for="firstname" class="form-label">First Name</label>
-                <input required type="text" class="form-control" id="firstname" name="firstname">
-            </div>
-            <div class="mb-3">
-                <label for="lastname" class="form-label">Last Name</label>
-                <input required type="text" class="form-control" id="lastname" name="lastname">
-            </div>
-            <div class="mb-3">
-                <label for="dob" class="form-label">Date of Birth</label>
-                <input type="text" class="form-control" id="dob" name="dob">
-            </div>
-            <div class="mb-3">
-                <label for="specialty" class="form-label">Sporting House</label>
-                <select class="form-control" id="specialty" name="specialty">
-                  
-                <?php while($r = $result->fetch(PDO::FETCH_ASSOC)) {?>
-                    <option value="<?php echo $r['specialty_id']?>" ><?php echo $r['name'];?></option>
-                    <?php }?>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="team" class="form-label">Sporting Team</label>
-                <input type="text" class="form-control" id="team" name="team">
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email Address</label>
-                <input required type="email" class="form-control" id="email" name="email">
-                <small id="emailHelp" class="form-text text-mute">Your email will remain private with Us!</small>
-            </div>
-            <div class = "form-group">
-            <div class="mb-3">
-                <label for="gender" class="form-label">Gender</label>
-                <input type="text" class="form-control" id="gender" name="gender">
-            </div>
-            </div>
-    
-           <div class="custom-file">
-                <label for="avatar" >Upload Image </label>
-                <input type="file" accept="image/*"class="custom-file-input" id="avatar" name="avatar">
-                <label class="custom-file-label" ></label> <br>
-                <small id="imageHelp" class="form-text text-mute">File Upload Optional</small>
+<div class="mb-3">
+    <label for="firstname" class="form-label">First Name</label>
+    <input required type="text" class="form-control" id="firstname" name="firstname">
+</div>
+<div class="mb-3">
+    <label for="lastname" class="form-label">Last Name</label>
+    <input required type="text" class="form-control" id="lastname" name="lastname">
+</div>
+<div class="mb-3">
+    <label for="dob" class="form-label">Date of Birth</label>
+    <input type="text" class="form-control" id="dob" name="dob">
+</div>
+<div class="mb-3">
+    <label for="specialty" class="form-label">Sporting House</label>
+    <select class="form-control" id="specialty" name="specialty">
+      
+    <?php while($r = $result->fetch(PDO::FETCH_ASSOC)) {?>
+        <option value="<?php echo $r['specialty_id']?>" ><?php echo $r['name'];?></option>
+        <?php }?>
+    </select>
+</div>
+<div class="mb-3">
+    <label for="team" class="form-label">Sporting Team</label>
+    <input type="text" class="form-control" id="team" name="team">
+</div>
+<div class="mb-3">
+    <label for="email" class="form-label">Email Address</label>
+    <input required type="email" class="form-control" id="email" name="email">
+    <small id="emailHelp" class="form-text text-mute">Your email will remain private with Us!</small>
+</div>
+<div class = "form-group">
+<div class="mb-3">
+    <label for="gender" class="form-label">Gender</label>
+    <input type="text" class="form-control" id="gender" name="gender">
+</div>
+</div>
 
-            </div>
-            <br>
-           <div class="d-grid gap-2">
-            <button type="submit" class="btn btn-primary " name="submit">Submit</button>
-           </div>
+<div class="custom-file">
+    <label for="avatar" >Upload Image </label>
+    <input type="file" accept="image/*"class="custom-file-input" id="avatar" name="avatar">
+    <label class="custom-file-label" ></label> <br>
+    <small id="imageHelp" class="form-text text-mute">File Upload Optional</small>
 
-        </form>  
+</div>
+<br>
+<div class="d-grid gap-2">
+<button type="submit" class="btn btn-primary " name="submit">Submit</button>
+</div>
+
+</form>  
+
+     </div>
+        
         <br>
         <br>
         <br>
